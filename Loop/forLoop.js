373f43e3f292ce6874c356687bd0c1016858e65e
omarfaruk70 <faruk1611281892@gmail.com>
2022-09-23 02:08:23 +0600

    for and while loop done

---
+++ Loop/forLoop.js
@@ -0,0 +1,14 @@
+// loop 
+// loop in an statement which is completed a particular task again and again or untill when its successfully 
+for(let i = 1; i < 10; i++){
+    console.log('amar sonar bangla ami tomay valobashi.')   // print it 10 times
+}
+// print all number (1 to 100)
+
+for(let i = 1; i <= 100; i++){
+    console.log(i)
+}
+// print all even number (2 - 50)
+for(let i = 2; i <= 50; i += 2){
+    console.log(i)
+}
---
+++ Loop/whileLoop.js
@@ -0,0 +1,22 @@
+// while loop.
+// while mane jotokkkhon
+// while loop mane holo statement ta totokkhon colbe jotokkkhon condition ta match na hobe.
+let i = 0;
+while( i < 10){
+    console.log('amar sonar bangla')
+    i++;
+};
+// print 1 to 100;
+let j = 0;
+while(j <= 100){
+    console.log(j)
+    j++
+};
+// getting sum of 0+1+2+3+4+5+6+7+8+9+10;
+let k  = 0;
+let sum = 0;
+while(k <= 10){
+    sum = sum + i;
+    k++
+}
+console.log('the result of the sum is  ', sum)
